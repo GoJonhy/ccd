2bc7e8be8faa45c82967b5abf58939c2c8c05448
GoJonhy <joao.lourenco.silva.96@gmail.com>
2021-01-23 18:26:19 +0000

    Adding Result Files

--- Trab2/Ponto7/bashRun.sh
+++ Trab2/Ponto7/bashRun.sh
@@ -3,12 +3,44 @@
 clear
 rm results*.txt
 
-for file in ./files/*
+for file in ./files/enron_mail_inbox/*
 do
-  python3 ../../compressUtils/compress_zlib.py -i "$file" -d "files/dict_reduced_lenac_32kiB.txt" >> results_zlib.txt
+  python3 ../compressUtils/compress_zlib.py -i "$file" -d "./heuritic_dict.txt" >> results_zlib_enron_mail.txt
 done
 
-for file in ../files/*
+for file in ./files/enron_mail_inbox/*
 do
-  python3 ../../compressUtils/compress_zlib_zdict.py -i "$file" -d "files/dict_reduced_lenac_32kiB.txt" >> results_zlib_zdict.txt
+  python3 ../compressUtils/compress_zlib_zdict.py -i "$file" -d "./heuritic_dict.txt" >> results_zlib_zdict_enron_mail.txt
+done
+
+for file in ./files/enron_mail_inbox/*
+do
+  python3 ../compressUtils/compress_lz4.py -i "$file" -d "./heuritic_dict.txt" >> results_lz4_enron_mail.txt
+done
+
+for file in ./files/enron_mail_inbox/*
+do
+  python3 ../compressUtils/compress_zstd.py -i "$file" -d "./heuritic_dict.txt" >> results_zstd_enron_mail.txt
+done
+
+# ----------
+
+for file in ../Ponto1/files/*
+do
+  python3 ../compressUtils/compress_zlib.py -i "$file" -d "./heuritic_dict.txt" >> results_zlib_Ponto1_files.txt
+done
+
+for file in ../Ponto1/files/*
+do
+  python3 ../compressUtils/compress_zlib_zdict.py -i "$file" -d "./heuritic_dict.txt" >> results_zlib_zdict_Ponto1_files.txt
+done
+
+for file in ../Ponto1/files/*
+do
+  python3 ../compressUtils/compress_lz4.py -i "$file" -d "./heuritic_dict.txt" >> results_lz4_Ponto1_files.txt
+done
+
+for file in ../Ponto1/files/*
+do
+  python3 ../compressUtils/compress_zstd.py -i "$file" -d "./heuritic_dict.txt" >> results_zstd_Ponto1_files.txt
 done
